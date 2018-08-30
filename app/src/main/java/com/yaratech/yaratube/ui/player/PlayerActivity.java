619240201449078480d0295f4e728d64bc00f68c
MohammadSianaki <m.rsianaki@yahoo.com>
2018-08-30 16:01:09 +0430

    activity player orientation changed to landscape

--- app/src/main/java/com/yaratech/yaratube/ui/player/PlayerActivity.java
+++ app/src/main/java/com/yaratech/yaratube/ui/player/PlayerActivity.java
@@ -1,32 +1,131 @@
 package com.yaratech.yaratube.ui.player;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.hls.HlsMediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 import com.yaratech.yaratube.R;
 
-public class PlayerActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class PlayerActivity extends AppCompatActivity {
+    private static final String TAG = "PlayerActivity";
     private static final String KEY_PRODUCT_FILE = "KEY_PRODUCT_FILE";
+    private SimpleExoPlayer player;
+
+    @BindView(R.id.player_fragment_player_view)
+    PlayerView playerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: ");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        setFragment();
+        ButterKnife.bind(this);
+        String videoUrl = getIntent().getExtras().getString(KEY_PRODUCT_FILE);
+        initExoPlayer(videoUrl);
+
+
     }
 
-    private void setFragment() {
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(
-                        R.id.player_activity_main_content,
-                        PlayerFragment.newInstance(getIntent().getExtras().getString(KEY_PRODUCT_FILE)),
-                        PlayerFragment.class.getSimpleName())
-                .commit();
+    private void initExoPlayer(String videoUrl) {
+
+        Handler mainHandler = new Handler();
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveTrackSelection.Factory(bandwidthMeter);
+        TrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
+
+        // 2. Create a default LoadControl
+        LoadControl loadControl = new DefaultLoadControl();
+        playerView.requestFocus();
+        player = ExoPlayerFactory.newSimpleInstance(this, new DefaultTrackSelector());
+        DefaultDataSourceFactory dataSourceFactory = new DefaultDataSourceFactory(
+                this,
+                Util.getUserAgent(this, getResources().getString(R.string.app_name)));
+
+
+        HlsMediaSource mediaSource = new HlsMediaSource
+                .Factory(dataSourceFactory)
+                .createMediaSource(Uri.parse(videoUrl));
+        player.prepare(mediaSource);
+        player.setPlayWhenReady(true);
+        playerView.setPlayer(player);
     }
 
+    @Override
+    protected void onRestart() {
+        Log.d(TAG, "onRestart: ");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart: ");
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        Log.d(TAG, "onRestoreInstanceState: ");
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume: ");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause: ");
+        super.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        Log.d(TAG, "onSaveInstanceState: ");
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop: ");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy: ");
+        releasePlayer();
+        super.onDestroy();
+    }
+
+    private void releasePlayer() {
+        if (player != null) {
+            player.stop();
+            player.release();
+            player = null;
+        }
+    }
 }
